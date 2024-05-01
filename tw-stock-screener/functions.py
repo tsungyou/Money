5aadcbf434bb3e6438d8769526f5a9b2d003aa7a
test <test@appledeMacBook-Pro.local>
2024-05-01 18:07:10 +0800

    first strat

--- tw-stock-screener/functions.py
+++ tw-stock-screener/functions.py
@@ -69,3 +69,69 @@ def strat1_vol_surge_cor_dd(ticker='6230.TW', start='2024-01-01', end='2024-04-3
 
     plt.tight_layout()
     plt.show()
+    return df
+
+
+def backtest_print_all(df):
+    start = 10000.0
+    ind = df[df['signal_vol_ma_final'] == True].index
+    print(ind)
+    cur = pd.Series([0] * len(df))
+    cur[0] = start
+    open_position = []
+    close_position = []
+    current = start
+    holding = False
+    df['pct_change'] = df['Close'].pct_change()
+    k = 0
+    pct_change_all = 0
+    for i in range(len(df)):
+        if df['signal_vol_ma_final'].iloc[i] == False and holding == False:
+            cur[i] = current
+            continue
+        else:
+            if holding == False:
+                open_position.append([i, df['Close'].iloc[i]])
+                buy_price = df['Close'].iloc[i]
+                cur[i] = current
+                holding = True
+            elif holding == True:
+                if df['signal_vol_ma_final'].iloc[i] == True:
+                    k = 0
+                pct_change = df['pct_change'].iloc[i]
+                pct_change_all += pct_change
+                current_open = current * (1+pct_change_all)
+                # print(current, pct_change, pct_change_all, current_open, i)
+                if k == 10:
+                    close_position.append([i, df['Close'].iloc[i]])
+                    holding = False
+                    current = current_open
+                    k = 0
+                cur[i] = current_open
+                
+                k += 1
+    if k != 1:
+        close_position.append([i, df['Close'].iloc[i], k-1])
+    plt.plot(cur)
+    plt.legend()
+    for i in open_position:
+        plt.axvline(i[0], color='r')
+    for i in close_position:
+        plt.axvline(i[0], color='b')
+
+    print(open_position)
+    print(close_position)
+    print(f"return: {cur[len(df)-1]/cur[0] - 1}")
+    print(cur[len(df) - 1])
+
+def backtest_return_data_only(df):
+    print(df)
+    return 0
+
+
+def loop_all_ticker_with_spec_strat(function = strat1_vol_surge_cor_dd, ticker='1234.TW'):
+    df = strat1_vol_surge_cor_dd(ticker)
+    print(df)
+
+if __name__ == "__main__":
+    loop_all_ticker_with_spec_strat()
