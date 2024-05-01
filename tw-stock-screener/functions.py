157c568a87a7a277adeade3c43d25ea3c31fb022
test <test@appledeMacBook-Pro.local>
2024-05-01 22:55:46 +0800

    bt function

--- tw-stock-screener/functions.py
+++ tw-stock-screener/functions.py
@@ -2,8 +2,16 @@ import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-def strat1_vol_surge_cor_dd(ticker='6230.TW', start='2024-01-01', end='2024-04-30'):
-    fig, ax = plt.subplots(5, 1, figsize=(6, 8))
+
+def strat1_bt(ticker='1513.TW', start='2023-01-01', end='2024-04-30'):
+    df = data_strat1(ticker, start, end)
+    return_total, winrate, avg_return = nav_returns_backtest_only(df)
+    print("============")
+    print(ticker)
+    print("return_total", return_total)
+    print("winrate", winrate)
+    print("avg_return", avg_return)
+def data_strat1(ticker, start, end):
     price_ma1 = 20
     price_ma2 = 60
     df = yf.download(ticker, start=start, end=end, interval='1d', progress=0)
@@ -23,13 +31,6 @@ def strat1_vol_surge_cor_dd(ticker='6230.TW', start='2024-01-01', end='2024-04-3
 
 
 
-    ax[0].set_title(f"{ticker}--{start}~{end}")
-    ax[0].plot(df['day_count'], df['Close'], label='Close')
-    ax[0].plot(df['day_count'], df[f'price_ma{price_ma1}'], label='ma1')
-    ax[0].plot(df['day_count'], df[f'price_ma{price_ma2}'], label='ma2')
-    for dates in df[df[f'price_ma{price_ma1}_low'] == 1]['day_count']:
-        ax[0].axvline(dates, color='black', alpha=0.3)
-
     # detact volume surge
     df['vol_max20'] = df['Volume'].rolling(window=15, min_periods=1).max()
     df['vol_max20_shift5'] = df['vol_max20'].shift(5)
@@ -39,7 +40,21 @@ def strat1_vol_surge_cor_dd(ticker='6230.TW', start='2024-01-01', end='2024-04-3
     # 出現超過15天低於15前的最大量，並且還沒出現更大量
     df['vol_under_max20_15d'] = df['vol_max20_shift5_over'].rolling(window=15, min_periods=1).apply(lambda x: all(x == False))
 
-    
+    df['signal_vol'] = (df['vol_under_max20_15d'] == True) & (df['vol_max20_over3d_past_1m'] == True)
+    df['signal_vol_ma_final'] = (df['signal_vol'] == True) & (df[f"price_ma{price_ma1}_low"] == True)
+    return df
+def strat1_vol_surge_cor_dd(ticker='6230.TW', start='2024-01-01', end='2024-04-30'):
+    price_ma1 = 20
+    price_ma2 = 60
+    fig, ax = plt.subplots(5, 1, figsize=(6, 8))
+    df = data_strat1(ticker, start, end)
+
+    ax[0].set_title(f"{ticker}--{start}~{end}")
+    ax[0].plot(df['day_count'], df['Close'], label='Close')
+    ax[0].plot(df['day_count'], df[f'price_ma{price_ma1}'], label='ma1')
+    ax[0].plot(df['day_count'], df[f'price_ma{price_ma2}'], label='ma2')
+    for dates in df[df[f'price_ma{price_ma1}_low'] == 1]['day_count']:
+        ax[0].axvline(dates, color='black', alpha=0.3)
     # ====== volume data
     ax[2].bar(df['day_count'], df['Volume'])
     ax[2].plot(df['day_count'], df['vol_max20_shift5'], color='r')
@@ -53,33 +68,36 @@ def strat1_vol_surge_cor_dd(ticker='6230.TW', start='2024-01-01', end='2024-04-3
         ax[3].axvline(dates, color='red', alpha=0.3)  
     ax[3].set_title("volume correction")
 
-    df['signal_vol'] = (df['vol_under_max20_15d'] == True) & (df['vol_max20_over3d_past_1m'] == True)
+    
     ax[4].plot(df['day_count'], df['Close'])
     for dates in df[df['signal_vol'] == 1]['day_count']:
         ax[4].axvline(dates, color='red', alpha=0.3)  
     ax[4].set_title("volume signal")
 
     # ======== Final Signal
-    df['signal_vol_ma_final'] = (df['signal_vol'] == True) & (df[f"price_ma{price_ma1}_low"] == True)
     
-    ax[1].plot(df['day_count'], df['Close'])
+    
+    # ax[1].plot(df['day_count'], df['Close'])
     for dates in df[df['signal_vol_ma_final'] == 1]['day_count']:
-        ax[1].axvline(dates, color='red', alpha=0.3)  
-    ax[1].set_title("signal total") 
+        ax[0].axvline(dates, color='red', alpha=0.6)  
+    # ax[1].set_title("signal total") 
+
+    nav_returns_subplots(ax[1], df)
+
 
     plt.tight_layout()
     plt.show()
     return df
 
-
-def backtest_print_all(df):
+def nav_returns_backtest_only(df):
     start = 10000.0
-    ind = df[df['signal_vol_ma_final'] == True].index
-    print(ind)
+    # ind = df[df['signal_vol_ma_final'] == True].index
+    # print(ind)
     cur = pd.Series([0] * len(df))
     cur[0] = start
     open_position = []
     close_position = []
+    returns_pertrade = []
     current = start
     holding = False
     df['pct_change'] = df['Close'].pct_change()
@@ -91,8 +109,8 @@ def backtest_print_all(df):
             continue
         else:
             if holding == False:
-                open_position.append([i, df['Close'].iloc[i]])
                 buy_price = df['Close'].iloc[i]
+                open_position.append([i, buy_price])
                 cur[i] = current
                 holding = True
             elif holding == True:
@@ -103,26 +121,105 @@ def backtest_print_all(df):
                 current_open = current * (1+pct_change_all)
                 # print(current, pct_change, pct_change_all, current_open, i)
                 if k == 10:
-                    close_position.append([i, df['Close'].iloc[i]])
+                    close_price = df['Close'].iloc[i]
+                    close_position.append([i, close_price])
                     holding = False
                     current = current_open
                     k = 0
+                    pct_change_all = 0
+                    returns_pertrade.append(close_price/buy_price - 1)
                 cur[i] = current_open
                 
                 k += 1
     if k != 1:
         close_position.append([i, df['Close'].iloc[i], k-1])
-    plt.plot(cur)
-    plt.legend()
-    for i in open_position:
-        plt.axvline(i[0], color='r')
-    for i in close_position:
-        plt.axvline(i[0], color='b')
+    return_total = cur[len(df)-1]/cur[0] - 1
+    avg_return = np.mean(returns_pertrade)
+    winrate = count_positive_numbers(returns_pertrade)/len(returns_pertrade)
+    return return_total, winrate, avg_return
 
-    print(open_position)
-    print(close_position)
-    print(f"return: {cur[len(df)-1]/cur[0] - 1}")
-    print(cur[len(df) - 1])
+def nav_returns_subplots(ax, df, data_output = True):
+    start = 10000.0
+    # ind = df[df['signal_vol_ma_final'] == True].index
+    # print(ind)
+    cur = pd.Series([0] * len(df))
+    cur[0] = start
+    open_position = []
+    close_position = []
+    returns_pertrade = []
+    current = start
+    holding = False
+    df['pct_change'] = df['Close'].pct_change()
+    k = 0
+    pct_change_all = 0
+    for i in range(len(df)):
+        if df['signal_vol_ma_final'].iloc[i] == False and holding == False:
+            cur[i] = current
+            continue
+        else:
+            if holding == False:
+                buy_price = df['Close'].iloc[i]
+                open_position.append([i, buy_price])
+                cur[i] = current
+                holding = True
+            elif holding == True:
+                if df['signal_vol_ma_final'].iloc[i] == True:
+                    k = 0
+                pct_change = df['pct_change'].iloc[i]
+                pct_change_all += pct_change
+                current_open = current * (1+pct_change_all)
+                # print(current, pct_change, pct_change_all, current_open, i)
+                if k == 10:
+                    close_price = df['Close'].iloc[i]
+                    close_position.append([i, close_price])
+                    holding = False
+                    current = current_open
+                    k = 0
+                    pct_change_all = 0
+                    returns_pertrade.append(close_price/buy_price - 1)
+                cur[i] = current_open
+                
+                k += 1
+    if k != 1:
+        close_position.append([i, df['Close'].iloc[i], k-1])
+
+    # data output calculation
+
+    return_total = cur[len(df)-1]/cur[0] - 1
+    avg_return = np.mean(returns_pertrade)
+    winrate = count_positive_numbers(returns_pertrade)/len(returns_pertrade)
+    if data_output:
+        ax.plot(df['day_count'], cur, label='returns')
+        for index, i in enumerate(open_position):
+            if index == 0:
+                ax.axvline(i[0], color='r', label='Open')
+            else:
+                ax.axvline(i[0], color='r')
+        for index, i in enumerate(close_position):
+            if index == 0:
+                ax.axvline(i[0], color='b', label='Close', alpha=0.3)
+            else:
+                ax.axvline(i[0], color='b')
+        ax.legend()
+        
+        print("returns: ", returns_pertrade)
+        print("avg per trade: ", avg_return)
+        print("winrate: ", winrate)
+        print(open_position)
+        print(close_position)
+        print(f"return: {return_total}")
+        print(cur[len(df) - 1])
+        return None
+    else:
+        return return_total, winrate, avg_return
+
+
+def count_positive_numbers(numbers):
+    count = 0
+    for num in numbers:
+        if num > 0:
+            count += 1
+    return count
 
 def backtest_return_data_only(df):
     print(df)
@@ -130,8 +227,9 @@ def backtest_return_data_only(df):
 
 
 def loop_all_ticker_with_spec_strat(function = strat1_vol_surge_cor_dd, ticker='1234.TW'):
-    df = strat1_vol_surge_cor_dd(ticker)
+    df = function(ticker)
     print(df)
 
 if __name__ == "__main__":
-    loop_all_ticker_with_spec_strat()
+    # df = strat1_vol_surge_cor_dd(ticker = "1513.TW", start='2023-01-01')
+    strat1_bt(ticker = "1513.TW", start='2023-01-01')
