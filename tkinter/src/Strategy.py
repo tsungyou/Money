c6e23d60cae1dbc0e7159a449503ac2a9a59a695
test <test@appledeMacBook-Pro.local>
2024-05-21 15:23:04 +0800

    a

--- tkinter/src/Strategy.py
+++ tkinter/src/Strategy.py
@@ -13,35 +13,12 @@ class Strategy(ctk.CTkFrame):
         self.inputBox1 = ctk.CTkEntry(self, placeholder_text='Stock symbol')
         self.inputBox1.pack(padx=10, pady=10)
 
-        self.inputButton1 = ctk.CTkButton(self, text='Show Plot', command=self.plotting)
-        self.inputButton1.pack(padx=30, pady=10)
-        self.inputButton1.bind("<Return>", self.plotting)
         # generate the figure and plot object which will be linked to the root elemen
 
+        self.stratMenu = ctk.CTkOptionMenu(self, values=['strat1', "strat2", 'strat3'], command=self.get_strat_result)
+        self.stratMenu.pack()
         # initiate the window
 
-    def plotting(self):
-        ticker = self.inputBox1.get()
+    def get_strat_result(self):
+        strat = self.stratMenu.get()
         
-        try:
-            tw = self.pq_loc_dir + f"{ticker}_TW.parquet"
-            df = pd.read_parquet(tw)
-        except:
-            try:
-                two = self.pq_loc_dir + f"{ticker}_TWO.parquet"
-                df = pd.read_parquet(two)
-            except:
-                print("no symbol existed")
-        
-        if self.canvas:
-            self.canvas.get_tk_widget().destroy()
-            self.canvas = None
-
-        fig, ax = plt.subplots()
-        fig.set_size_inches(8,4)
-        ax.plot(df["Close"])
-        ax.axis("off")
-        fig.subplots_adjust(left=0, right=1, bottom=0, top=1, wspace=0, hspace=0)
-        self.canvas = FigureCanvasTkAgg(fig,master=self)
-        self.canvas.draw()
-        self.canvas.get_tk_widget().pack(pady=0.15)
