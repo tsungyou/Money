e612088bc10c1aabb01ec772fe4c45ddf6220481
test <test@appledeMacBook-Pro.local>
2024-05-26 14:21:47 +0800

    update tkinter

--- tkinter/src/Cluster.py
+++ tkinter/src/Cluster.py
@@ -1,10 +1,137 @@
 import customtkinter as ctk
+from tkinter import ttk
+from tkinter import font
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
 class Cluster(ctk.CTkFrame):
+    
     def __init__(self, parent):
         super().__init__(parent)
-        label = ctk.CTkLabel(self, text="Welcome to the Portfolio Page")
-        label.pack(pady=20)
 
-        input1 = ctk.CTkEntry(self, placeholder_text='20')
-        input1.pack(pady=20)
+        # Configure the grid layout
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=1)
+        self.grid_rowconfigure(2, weight=1)
 
+        # Configure buttons
+        self.surge_button = ctk.CTkButton(self, text='漲幅排行', command=self.insert_tree, width=100, height=50)
+        self.surge_button.grid(row=0, column=0, padx=2, pady=2, sticky='n')
+
+        self.down_button = ctk.CTkButton(self, text='跌幅排行', command=self.insert_tree1, width=100, height=50)
+        self.down_button.grid(row=0, column=1, padx=2, pady=2, sticky='n')
+
+        # Configure treeview
+        style = ttk.Style()
+        style.configure("Treeview.Heading", font=("Helvetica", 20))
+        style.configure("Treeview", rowheight=50, font=("Helvetica", 20))
+
+        self.tree = ttk.Treeview(self, columns=("Symbol", "Price", "Percentage"), show="headings", style="Treeview")
+        self.tree.heading("Symbol", text="Symbol")
+        self.tree.heading("Price", text="Price")
+        self.tree.heading("Percentage", text="Percentage")
+        self.tree.grid(row=1, column=0, padx=2, pady=2, sticky='nsew')
+
+        self.tree1 = ttk.Treeview(self, columns=("Symbol", "Price", "Percentage"), show="headings", style="Treeview")
+        self.tree1.heading("Symbol", text="Symbol")
+        self.tree1.heading("Price", text="Price")
+        self.tree1.heading("Percentage", text="Percentage")
+        self.tree1.grid(row=1, column=1, padx=2, pady=2, sticky='nsew')
+
+        self.pack(expand=True, fill='both')
+
+
+    def insert_tree(self):
+        df = self.get_surge()
+        self.tree.delete(*self.tree.get_children())
+        for i in range(len(df)):
+            self.tree.insert("", "end", values=(df.iloc[i, 0], df.iloc[i, 1], df.iloc[i, 3]))
+    def get_surge(self):
+            # 目标URL
+        url = 'https://tw.stock.yahoo.com/rank/change-up'
+
+        # 发送请求获取网页内容
+        response = requests.get(url)
+        if response.status_code != 200:
+            print('Failed to retrieve data')
+            exit()
+
+        # 使用BeautifulSoup解析HTML
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        s = soup.find_all("div", {"class": "Pos(r) Ov(h)"})
+        s1 = soup.find_all("li", class_="List(n)")
+        #df = pd.DataFrame()
+        data = []
+        for i in s1:
+            s = str(i).split(">")
+            indices = [14, 17, 24, 30, 36, 40, 44, 48, 56]
+
+            # 提取指定索引的元素
+            try:
+                col1 = [s[k] for k in indices]
+            except IndexError as e:
+                print(f"IndexError: {e} - Some indices are out of range for the split list.")
+
+            col1 = [q.split("<")[0] for q in col1]
+            data.append(col1[1:])
+            #df[col1[0]] = col1[1:]
+            # 打印结果
+
+        #df_close = df.T
+        header = ['股號', '股價', '漲跌', '漲跌幅(%)','最高','最低','價差', '成交金額(億)']
+        #df_close.columns = ['股號', '股價', '漲跌', '漲跌幅(%)','最高','最低','價差', '成交金額(億)']
+        df = pd.DataFrame(data, columns=header)
+        numeric_columns = ['股價', '漲跌', '最高','最低','價差', '成交金額(億)']
+        for column in numeric_columns:
+            df[column] = df[column].str.replace(',', '')
+        return df
+
+    def insert_tree1(self):
+        df = self.get_down()
+        self.tree1.delete(*self.tree1.get_children())
+        for i in range(len(df)):
+            self.tree1.insert("", "end", values=(df.iloc[i, 0], df.iloc[i, 1], df.iloc[i, 3]))
+    def get_down(self):
+            # 目标URL
+        url = 'https://tw.stock.yahoo.com/rank/change-down'
+
+        # 发送请求获取网页内容
+        response = requests.get(url)
+        if response.status_code != 200:
+            print('Failed to retrieve data')
+            exit()
+
+        # 使用BeautifulSoup解析HTML
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        s = soup.find_all("div", {"class": "Pos(r) Ov(h)"})
+        s1 = soup.find_all("li", class_="List(n)")
+        #df = pd.DataFrame()
+        data = []
+        for i in s1:
+            s = str(i).split(">")
+            indices = [14, 17, 24, 30, 36, 40, 44, 48, 56]
+
+            # 提取指定索引的元素
+            try:
+                col1 = [s[k] for k in indices]
+            except IndexError as e:
+                print(f"IndexError: {e} - Some indices are out of range for the split list.")
+
+            col1 = [q.split("<")[0] for q in col1]
+            data.append(col1[1:])
+            #df[col1[0]] = col1[1:]
+            # 打印结果
+
+        #df_close = df.T
+        header = ['股號', '股價', '漲跌', '漲跌幅(%)','最高','最低','價差', '成交金額(億)']
+        #df_close.columns = ['股號', '股價', '漲跌', '漲跌幅(%)','最高','最低','價差', '成交金額(億)']
+        df = pd.DataFrame(data, columns=header)
+        numeric_columns = ['股價', '漲跌', '最高','最低','價差', '成交金額(億)']
+        for column in numeric_columns:
+            df[column] = df[column].str.replace(',', '')
+        return df
--- tkinter/src/HomePage.py
+++ tkinter/src/HomePage.py
@@ -1,11 +1,60 @@
 import customtkinter as ctk
-from tkinter import ttk
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
 class HomePage(ctk.CTkFrame):
     def __init__(self, parent):
         super().__init__(parent)
+        self.canvas = None
+        self.pq_loc_dir = "../tw-stock-screener/database/"
 
-        self.tree = ttk.Treeview(self, columns=("Key", "Database", "Collection"), show="headings")
-        self.tree.heading("Key", text="Key")
-        self.tree.heading("Database", text="Database")
-        self.tree.heading("Collection", text="Collection")
-        self.tree.pack(pady=80)
+        self.inputBox1 = ctk.CTkEntry(self, placeholder_text='Stock symbol')
+        self.inputBox1.pack(padx=10, pady=10)
+
+
+        self.inputBox_ma = ctk.CTkEntry(self, placeholder_text='ma')
+        self.inputBox_ma.pack(padx=10, pady=20)
+
+        self.inputButton1 = ctk.CTkButton(self, text='Show Plot', command=self.plotting)
+        self.inputButton1.pack(padx=30, pady=10)
+        self.inputButton1.bind("<Return>", self.plotting)
+
+        self.menu_ma = ctk.CTkOptionMenu(self, values=["20", "60", "100"], command=self.plotting)
+        self.menu_ma.pack(padx=30, pady=40)
+        # generate the figure and plot object which will be linked to the root elemen
+
+        # initiate the window
+
+    def plotting(self):
+        ticker = self.inputBox1.get()
+        ma = self.menu_ma.get()
+        try:
+            tw = self.pq_loc_dir + f"{ticker}_TW.parquet"
+            df = pd.read_parquet(tw)
+        except:
+            try:
+                two = self.pq_loc_dir + f"{ticker}_TWO.parquet"
+                df = pd.read_parquet(two)
+            except:
+                print("no symbol existed")
+        
+        if self.canvas:
+            self.canvas.get_tk_widget().destroy()
+            self.canvas = None
+
+        df['ma'] = df['Close'].rolling(int(ma)).mean()
+
+        fig, ax = plt.subplots()
+        fig.set_size_inches(8,4)
+        ax.plot(df['ma'], label='ma')
+        ax.plot(df["Close"], label='close')
+        ax.legend()
+        ax.axis("off")
+        fig.subplots_adjust(left=0, right=1, bottom=0, top=1, wspace=0, hspace=0)
+        self.canvas = FigureCanvasTkAgg(fig,master=self)
+        self.canvas.draw()
+        self.canvas.get_tk_widget().pack(pady=0.15)
+
+plt.plot()
