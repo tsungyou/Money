7b2550298a3a7dd9b19a21ad727842be3c5da31d
test <test@appledeMacBook-Pro.local>
2024-06-02 20:11:48 +0800

    model backtest

--- tkinter/src/Backtester.py
+++ tkinter/src/Backtester.py
@@ -5,26 +5,33 @@ class Backtester(ctk.CTkFrame):
     def __init__(self, parent):
         super().__init__(parent)
 
-        self.tree = ttk.Treeview(self, columns=("Date", "Open", "Close"), show="headings")
-        self.tree.heading("Date", text="Date")
-        self.tree.heading("Open", text="Open")
-        self.tree.heading("Close", text="Close")
-        self.tree.pack(pady=40)
+        # params
+        self.holding_period_dict = {
+            '一週': 5,
+            '兩週': 10,
+            '一個月': 20, 
+            '三個月': 60, 
+            '六個月': 120
+        }
+        
+        self.label_symbol = ctk.CTkLabel(self, text="策略名稱（自取）")
+        self.label_symbol.grid(row=0, column=0, padx=10, pady=(5, 0))
+        self.inputBox1 = ctk.CTkEntry(self, placeholder_text='策略名稱')
+        self.inputBox1.grid(row=1, column=0, padx=10, pady=5)
+ 
+        self.label_ma = ctk.CTkLabel(self, text="移動平均")
+        self.label_ma.grid(row=0, column=1, padx=10, pady=(5, 0))
+        self.menu_ma = ctk.CTkEntry(self, placeholder_text="20")
+        self.menu_ma.grid(row=1, column=1, padx=10, pady=5)
 
-        self.label = ctk.CTkLabel(self, text="Welcome to the Settings Page")
-        self.label.pack(pady=0)
+        self.label_date = ctk.CTkLabel(self, text="持股期間（多久換一次股）")
+        self.label_date.grid(row=0, column=2, padx=10, pady=(5, 0))
+        self.menu_inputDate = ctk.CTkOptionMenu(self, values=['一週', '兩週', '一個月', '三個月', '六個月'])
+        self.menu_inputDate.grid(row=1, column=2, padx=10, pady=5)
 
-        self.input_stock = ctk.CTkEntry(self, placeholder_text='20')
-        self.input_stock.pack(pady=10)
-
-        self.button_stock = ctk.CTkButton(self, text='Search Stock', command=self.search_stock_pq)
-        self.button_stock.pack(pady=20)
-
-        self.button_delete = ctk.CTkButton(self, text="Delete", command=self.delete_textbox)
-        self.button_delete.pack(pady=30, padx=20)
-
-        self.textbox = ctk.CTkTextbox(self)
-        self.textbox.pack(pady=40)
+        self.inputButton1 = ctk.CTkButton(self, text='生成策略')
+        self.inputButton1.grid(row=1, column=3, padx=10, pady=5)
+        # Input Box and Buttons
 
     def search_stock_pq(self):
         stocks = self.input_stock.get()
--- tkinter/src/Searcher.py
+++ tkinter/src/Searcher.py
@@ -8,44 +8,28 @@ class Searcher(ctk.CTkFrame):
     def __init__(self, parent):
         super().__init__(parent)
 
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_columnconfigure(1, weight=1)
-        self.grid_columnconfigure(2, weight=1)
-        self.grid_columnconfigure(3, weight=1)
-        self.grid_columnconfigure(4, weight=1)
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_rowconfigure(1, weight=1)
-        self.grid_rowconfigure(2, weight=1)
-
         self.canvas = None
         self.pq_loc_dir = "../tw-stock-screener/database/"
 
         # Labels for each input
-        self.label_symbol = ctk.CTkLabel(self, text="Stock Symbol")
+        self.label_symbol = ctk.CTkLabel(self, text="股票代號")
         self.label_symbol.grid(row=0, column=0, padx=10, pady=(5, 0))
-
-        self.label_show_plot = ctk.CTkLabel(self, text="Plot Button")
-        self.label_show_plot.grid(row=0, column=2, padx=10, pady=(5, 0))
-
-        self.label_ma = ctk.CTkLabel(self, text="Moving Average")
-        self.label_ma.grid(row=0, column=3, padx=10, pady=(5, 0))
-
-        self.label_date = ctk.CTkLabel(self, text="Year")
-        self.label_date.grid(row=0, column=4, padx=10, pady=(5, 0))
-
-        # Input Box and Buttons
-        self.inputBox1 = ctk.CTkEntry(self, placeholder_text='Stock symbol')
+        self.inputBox1 = ctk.CTkEntry(self, placeholder_text='股票代號')
         self.inputBox1.grid(row=1, column=0, padx=10, pady=5)
 
-        self.inputButton1 = ctk.CTkButton(self, text='Show Plot', command=self.plotting)
-        self.inputButton1.grid(row=1, column=2, padx=10, pady=5)
-        self.inputButton1.bind("<Return>", self.plotting)
-
+        self.label_ma = ctk.CTkLabel(self, text="移動平均")
+        self.label_ma.grid(row=0, column=1, padx=10, pady=(5, 0))
         self.menu_ma = ctk.CTkOptionMenu(self, values=["20", "60", "120", "100"], command=self.plotting)
-        self.menu_ma.grid(row=1, column=3, padx=10, pady=5)
+        self.menu_ma.grid(row=1, column=1, padx=10, pady=5)
 
+        self.label_date = ctk.CTkLabel(self, text="開始年度")
+        self.label_date.grid(row=0, column=2, padx=10, pady=(5, 0))
         self.menu_inputDate = ctk.CTkOptionMenu(self, values=['2019', '2020', '2021', '2022', '2023', '2024'], command=self.plotting)
-        self.menu_inputDate.grid(row=1, column=4, padx=10, pady=5)
+        self.menu_inputDate.grid(row=1, column=2, padx=10, pady=5)
+
+        self.inputButton1 = ctk.CTkButton(self, text='顯示', command=self.plotting)
+        self.inputButton1.grid(row=1, column=3, padx=10, pady=5)
+        self.inputButton1.bind("<Return>", self.plotting)
 
         self.plotting()
 
